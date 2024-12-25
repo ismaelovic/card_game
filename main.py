e475eb9beb4aee26aab1691dd72c9e95e2379255
Ismael <ismaelz@MacBook-Pro-tilhrende-Ismael.local>
2024-12-25 17:59:25 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def run(user_input):
+    "Run the crew."
+    return print("Hello crew!")
+
+
+if __name__ == "__main__":
+    print("Hi dude, Im your Football Assistant for today!")
+    print("What would you like to know? Type 'quit' to kill me.")
